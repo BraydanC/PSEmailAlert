97d0665bd29c589d91d19604d1e8a455078442fc
Braydan C <braydanc@gmail.com>
2022-05-13 13:06:23 -0500

    Simple GPO script to output GPO settings to html

---
+++ GPOreport.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module ActiveDirectory
+
+Import-Module GroupPolicy
+
+$dc = Get-ADDomainController -Discover -Service PrimaryDC
+
+Get-GPOReport -All -Domain hottmannconstru.local -Server $dc -ReportType HTML -Path C:\Users\bcoffing\Desktop\GPOReportsAll.html
