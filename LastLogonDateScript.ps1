d08b626c0d12e58cbe8e3c4297890ecc130292fe
Braydan C <braydanc@gmail.com>
2022-05-16 14:56:21 -0500

    Used to check last login date/time to domain

---
+++ LastLogonDateScript.ps1
@@ -0,0 +1,8 @@
+﻿# I used this when I started to see last logon date of computer
+# to confirm if it's being used still, or was likely retired.
+
+$computers=import-csv C:\Users\user\filepath\location\for\pclist.csv
+foreach($computer in $computers){
+get-adcomputer $($computer.name) -properties "LastLogonDate" | Select Name, LastLogonDate 
+
+}
