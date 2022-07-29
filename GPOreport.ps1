56d7296599997f02cf6fda474732e2920351873e
Braydan C <braydanc@gmail.com>
2022-07-29 13:20:59 -0500

    Delete GPOreport.ps1

--- GPOreport.ps1
+++
@@ -1,8 +0,0 @@
-﻿Import-Module ActiveDirectory
-
-Import-Module GroupPolicy
-
-Remove-Item -path C:\filepath\GPOReportsAll.html
-$dc = Get-ADDomainController -Discover -Service PrimaryDC
-
-Get-GPOReport -All -Domain hottmannconstru.local -Server $dc -ReportType HTML -Path C:\Users\bcoffing\Desktop\GPOReportsAll.html
