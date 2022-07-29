71a41ce5c319ec308089c8227650f44026b9af93
Braydan C <braydanc@gmail.com>
2022-07-29 13:22:33 -0500

    Deletes old gpreport, creates new

---
+++ GPOreport.ps1
@@ -0,0 +1,8 @@
+﻿Import-Module ActiveDirectory
+
+Import-Module GroupPolicy
+
+Remove-Item -path C:\filepath\GPOReportsAll.html
+$dc = Get-ADDomainController -Discover -Service PrimaryDC
+
+Get-GPOReport -All -Domain *domain* -Server $dc -ReportType HTML -Path C:\filepath\GPOReportsAll.html
