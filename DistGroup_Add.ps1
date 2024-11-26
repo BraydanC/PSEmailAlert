86cf84f86d0090681e767a0e6713a1a51afd4f7f
Braydan C <braydanc@gmail.com>
2024-11-26 13:57:59 -0600

    Add Rooms to Distribution Group - Exchange Online
    #Import-Module ExchangeOnlineManagement in Powershell
    #Connect-ExchangeOnline to connect

---
+++ DistGroup_Add.ps1
@@ -0,0 +1,18 @@
+﻿$Rooms = @(
+"Room1@name.com",
+"Room2@name.com",
+"Room3@name.com",
+"Room4@name.com",
+"Room5@name.com"
+)
+
+$DistributionGroup = "Corp Rooms"
+
+foreach ($Room in $Rooms) {
+    try {
+        Add-DistributionGroupMember -Identity $DistributionGroup -Member $Room -ErrorAction Stop
+        Write-Host "Added $Room to $DistributionGroup successfully."
+    } catch {
+        Write-Host "Failed to add $Room to $DistributionGroup $_" 
+    }
+}
